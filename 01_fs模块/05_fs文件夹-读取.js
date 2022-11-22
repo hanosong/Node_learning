d0f923a35ad3b7d847ebfca788388db84212ea95
hanosong <838519800@qq.com>
2022-11-22 17:56:49 +0800

    upd：文件夹的常见操作

--- 01_fs模块/05_fs文件夹-读取.js
+++ 01_fs模块/05_fs文件夹-读取.js
@@ -7,13 +7,32 @@ const fs = require("fs");
 // });
 
 // 2. 读取文件夹，并且获取文件夹中文件的信息
-fs.readdir("./readtest", { withFileTypes: true }, (err, files) => {
-  console.log(files, "files");
-  /**
-   * 2: 文件夹， 1：文件
-   * [
-        Dirent { name: 'aaa', [Symbol(type)]: 2 },
-        Dirent { name: 'bbb', [Symbol(type)]: 2 }
-     ] 
-   */
-});
+// fs.readdir("./readtest", { withFileTypes: true }, (err, files) => {
+//   console.log(files, "files");
+//   /**
+//    * 2: 文件夹， 1：文件
+//    * [
+//         Dirent { name: 'aaa', [Symbol(type)]: 2 },
+//         Dirent { name: 'bbb', [Symbol(type)]: 2 }
+//      ]
+//    */
+// });
+
+// 2.2 如果文件夹里套了多层文件夹 --> 递归操作
+function readDirectory(path) {
+  fs.readdir(path, { withFileTypes: true }, (err, files) => {
+    files.forEach((item) => {
+      // 如果拿到的又是一个文件夹
+      if (item.isDirectory()) {
+        readDirectory(`${path}/${item.name}`);
+      } else {
+        console.log("获取到文件的名字是:", item.name);
+      }
+    });
+  });
+}
+
+readDirectory("./readtest");
+// 获取到文件的名字是: aaa.txt
+// 获取到文件的名字是: bbb.txt
+// 获取到文件的名字是: ccc.tst
---
+++ 01_fs模块/06_对文件夹进行重命名.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+// 1. 对文件夹进行重命名操作---(oldpath, newpath)
+// fs.rename("./rename", "./newname", (err) => {
+//   console.log(err, "重命名失败"); // null ---> 重命名成功
+// });
+
+// 2. 对文件进行重命名 ---> 同样的方法
